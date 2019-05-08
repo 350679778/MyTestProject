6ba89030b9bffd2d6b8ff436e04757b536c73033
王任重 <350679778@qq.com>
2019-05-08 09:34:21 +0800

    学习

--- e3-manager-web/src/main/java/cn/e3mall/controller/ItemController.java
+++ e3-manager-web/src/main/java/cn/e3mall/controller/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import cn.e3mall.common.pojo.EasyUIDataGridResult;
 import cn.e3mall.pojo.TbItem;
 import cn.e3mall.service.ItemService;
 
@@ -24,6 +25,16 @@ public class ItemController {
 		return tbItem;
 	}
 	
+	
+	@RequestMapping("/item/list")
+	@ResponseBody
+	public EasyUIDataGridResult getItemList(Integer page,Integer rows){
+		System.out.println("进入controller-getItemList---page:"+page+",rows:"+rows);
+		//调用服务查询商品列表
+		EasyUIDataGridResult result = itemService.getItemList(page, rows);
+		System.out.println("返回controller-getItemList---result:"+result.toString());
+		return result;
+	}
 	
 	
 }
---
+++ e3-manager-web/src/main/java/cn/e3mall/controller/PageController.java
@@ -0,0 +1,25 @@
+package cn.e3mall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 页面跳转controller
+ * @author 王任重
+ *
+ */
+@Controller
+public class PageController {
+	
+	
+	@RequestMapping("/")
+	public String showIndex(){
+		return "index";
+	}
+	
+	@RequestMapping("/{page}")
+	public String showPage(@PathVariable String page){
+		return page;
+	}
+}
